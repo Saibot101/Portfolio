f07158d6b46aae660b2a60c09f7f5bd9035e3920
Tobias Maas <tobiasmaas98@web.de>
2023-04-13 12:17:12 +0200

    vdsvdsv

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ const routes = [
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
       },
       {
-        path: '/portfolio',
+        path: '/port',
         name: 'Portfolio',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
